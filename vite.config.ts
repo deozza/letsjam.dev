63ed10f608e7dd04f064d23031c648f10631a1fd
Edenn Touitou <edenn.touitou@letsjam.fr>
2026-07-26 19:48:45 +0200

    [DEV] added remote function compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -7,10 +7,16 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit({
+			experimental: {
+				remoteFunctions: true
+			},
 			compilerOptions: {
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true,
+				experimental: {
+					async: true
+				},
 			},
 			adapter: adapter(),
 			extensions: ['.svelte']
